d4e421ddc146519d1de9b2423a2feab741139831
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-04-16 09:39:52 +0000

    prettier

--- StormOtto/servo-90.ino
+++ StormOtto/servo-90.ino
@@ -1,14 +1,16 @@
-// Include the Servo library 
-#include <Servo.h> 
-// Declare the Servo pin 
-int servoPin = 3; 
-// Create a servo object 
-Servo Servo1; 
-void setup() { 
-   // We need to attach the servo to the used pin number 
-   Servo1.attach(servoPin); 
+// Include the Servo library
+#include <Servo.h>
+// Declare the Servo pin
+int servoPin = 3;
+// Create a servo object
+Servo Servo1;
+void setup()
+{
+   // We need to attach the servo to the used pin number
+   Servo1.attach(servoPin);
 }
-void loop(){ 
+void loop()
+{
    Servo1.write(90); // make servo 90 degrees
-   delay(1000); // delay 1 sec
+   delay(1000);      // delay 1 sec
 }
